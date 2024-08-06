daa18c7e27b4d2a5e8054ad498e026f7061e07c0
HungChin <HungChin02244@gmail.com>
2024-08-06 10:53:59 +0800

    初始化設定Spring Boot Project

---
+++ springboot-mal/src/test/java/com/chin/springbootmal/SpringbootMalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chin.springbootmal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
